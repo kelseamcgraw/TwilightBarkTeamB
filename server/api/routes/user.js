e8a8ad13e6d6dcd7f4b386f0b3eb6783966b31b3
Michael <roesslma@miamioh.edu>
2019-11-08 12:17:04 -0500

    Verify account page

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -84,6 +84,43 @@ router.post("/signup", [userCreate(), validate], (req, res, next) => {
 
     } else {
 
+        //EMAIL HERE
+        var app = require('express')(),
+            mailer = require('express-mailer');
+
+        mailer.extend(app, {
+          from: 'roesslma@miamioh.edu',
+          host: 'smtp.gmail.com', // hostname
+          secureConnection: true, // use SSL
+          port: 465, // port for secure SMTP
+          transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+          auth: {
+            user: 'roesslma@miamioh.edu',
+            pass: '#ELLA3552!'
+          }
+        });
+
+        //app.set('views', __dirname + '/views');
+        //app.set('view engine', 'jade');
+
+        app.get('/', function (req, res, next) {
+          app.mailer.send('email', {
+            to: 'roesslma@miamioh.edu', // REQUIRED. This can be a comma delimited string just like a normal email to field.
+            subject: 'Test Email', // REQUIRED.
+            otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
+          }, function (err) {
+            if (err) {
+              // handle error
+              console.log(err);
+              res.send('There was an error sending the email');
+              return;
+            }
+            res.send('Email Sent');
+          });
+        });
+
+
+
         bcrypt.hash(req.body.password, hashCount, function(err, hash) {
         
         req.body.password = hash;
